1b41ba35dd89e29689538c76f590e790eec3b415
Jack Sundholm <jack.sundholm@student.oulu.fi>
2024-01-24 13:51:08 +0200

    updating for hw2

--- app/src/main/java/com/example/myapplication/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
-import androidx.compose.foundation.clickable
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
@@ -33,7 +34,7 @@ fun HomeScreen(navController: NavController){
 
 @Composable
 @Preview
-fun HomeScreeenPreview(){
+fun HomeScreenPreview(){
     HomeScreen(
         navController = rememberNavController()
     )
